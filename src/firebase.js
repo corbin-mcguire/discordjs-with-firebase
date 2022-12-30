dc1e0dfd905c5fa8a7260ae03cc4ead1dc9d155f
Corbin McGuire <mcguire.corbin@gmail.com>
2022-12-30 12:08:42 -0500

    Added firebase code

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+let firebaseApp = null;
+
+export const initializeFirebase = () => {
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  };
+  firebaseApp = initializeApp(firebaseConfig);
+};
+
+export const getFirebaseApp = () => {
+  if (!firebaseApp) initializeFirebase();
+  return getDatabase(firebaseApp);
+};
+
+// Your Firebase functions here
